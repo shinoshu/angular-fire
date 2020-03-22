0c86e17dd4dfdac2e217f6d90b4e6614ab19a8f1
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-22 19:56:37 +0900

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { AngularFireRemoteConfig } from '@angular/fire/remote-config';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-fire';
+  constructor(remoteConfig: AngularFireRemoteConfig) {
+    remoteConfig.parameters.subscribe(parameters => console.log(parameters));
+  }
 }
